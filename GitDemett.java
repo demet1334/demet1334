c01d115d8e8869504b40e0eda716a5a59ccef85b
GitDemet <demetckr89@gmail.com>
2024-02-13 13:50:00 +0000

    satin alma islemi onayi eklendi

--- GitDemett.java
+++ GitDemett.java
@@ -3,7 +3,10 @@ package GitDemett;
 public class GitDemett {
     public static void main(String[] args) {
         System.out.println("merhaba");
-        System.out.println("merhaba2");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("merhaba 5");
+
+        }
 
         for (int i = 0; i < 5; i++) {
             System.out.println("merhaba");
