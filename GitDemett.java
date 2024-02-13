b32940e1256d40817b007dcd03d020a56e8f75ca
GitDemet <demetckr89@gmail.com>
2024-02-13 13:23:48 +0000

    kredi karti kontrolu eklendi

--- GitDemett.java
+++ GitDemett.java
@@ -4,5 +4,6 @@ public class GitDemett {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("merhaba2");
+        System.out.println("merhaba3");
     }
 }
